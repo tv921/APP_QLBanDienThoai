de5d5ae25848b1f317f5cb5963ab4bf915a9ffb5
tv921 <trongprotv921@gmail.com>
2024-05-16 13:00:26 +0700

    cap nhat logic form dang ky

---
+++ DANGKY.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace QLCHXM.DangNhap
+{
+    partial class DANGKY
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_xacnhanmk = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.tb_matkhau = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_dangky = new System.Windows.Forms.Button();
+            this.tb_tentaikhoan = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_xacnhanmk
+            // 
+            this.tb_xacnhanmk.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_xacnhanmk.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_xacnhanmk.Location = new System.Drawing.Point(298, 401);
+            this.tb_xacnhanmk.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_xacnhanmk.Name = "tb_xacnhanmk";
+            this.tb_xacnhanmk.PasswordChar = '*';
+            this.tb_xacnhanmk.Size = new System.Drawing.Size(304, 44);
+            this.tb_xacnhanmk.TabIndex = 41;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 401);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(278, 49);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Re-enter password";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(90, 474);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 48);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Your email";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 48);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Enter password";
+            // 
+            // tb_email
+            // 
+            this.tb_email.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_email.Location = new System.Drawing.Point(298, 474);
+            this.tb_email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(304, 44);
+            this.tb_email.TabIndex = 42;
+            // 
+            // tb_matkhau
+            // 
+            this.tb_matkhau.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_matkhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_matkhau.Location = new System.Drawing.Point(298, 329);
+            this.tb_matkhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_matkhau.Name = "tb_matkhau";
+            this.tb_matkhau.PasswordChar = '*';
+            this.tb_matkhau.Size = new System.Drawing.Size(304, 44);
+            this.tb_matkhau.TabIndex = 40;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 48);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "User name";
+            // 
+            // bt_dangky
+            // 
+            this.bt_dangky.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.bt_dangky.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_dangky.Location = new System.Drawing.Point(370, 562);
+            this.bt_dangky.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bt_dangky.Name = "bt_dangky";
+            this.bt_dangky.Size = new System.Drawing.Size(156, 60);
+            this.bt_dangky.TabIndex = 38;
+            this.bt_dangky.Text = "SignUp";
+            this.bt_dangky.UseVisualStyleBackColor = false;
+            this.bt_dangky.Click += new System.EventHandler(this.bt_dangky_Click_2);
+            // 
+            // tb_tentaikhoan
+            // 
+            this.tb_tentaikhoan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_tentaikhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_tentaikhoan.Location = new System.Drawing.Point(298, 258);
+            this.tb_tentaikhoan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_tentaikhoan.Name = "tb_tentaikhoan";
+            this.tb_tentaikhoan.Size = new System.Drawing.Size(304, 44);
+            this.tb_tentaikhoan.TabIndex = 37;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QLCHXM.Properties.Resources.icon_user_slide__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(298, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 234);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 36;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DANGKY
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(768, 682);
+            this.Controls.Add(this.tb_xacnhanmk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.tb_matkhau);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_dangky);
+            this.Controls.Add(this.tb_tentaikhoan);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "DANGKY";
+            this.Text = "DANGKY";
+            this.Load += new System.EventHandler(this.DANGKY_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_xacnhanmk;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.TextBox tb_matkhau;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_dangky;
+        private System.Windows.Forms.TextBox tb_tentaikhoan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
