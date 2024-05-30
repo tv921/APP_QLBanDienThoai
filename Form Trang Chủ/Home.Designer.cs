ab3d56b3d995422a5b1cf6b6aaca3671d97bc094
tv921 <trongprotv921@gmail.com>
2024-05-30 15:18:17 +0700

    xu ly logic trang chu

---
+++ Form Trang Chủ/Home.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace QLCHXM
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.resetHome = new FontAwesome.Sharp.IconPictureBox();
+            this.panelChildForm_AD = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btn_khachhang_AD = new System.Windows.Forms.Button();
+            this.btn_nhacungcap_AD = new System.Windows.Forms.Button();
+            this.btn_donhang_AD = new System.Windows.Forms.Button();
+            this.btn_thongke_AD = new System.Windows.Forms.Button();
+            this.btn_nhanvien_AD = new System.Windows.Forms.Button();
+            this.btn_hanghoa_AD = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.iconButtonThoat = new FontAwesome.Sharp.IconButton();
+            this.btnDangXuat = new FontAwesome.Sharp.IconButton();
+            this.panelDesktop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resetHome)).BeginInit();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelDesktop.BackgroundImage = global::QLCHXM.Properties.Resources.Screenshot_2024_01_17_2332581;
+            this.panelDesktop.Controls.Add(this.resetHome);
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDesktop.Location = new System.Drawing.Point(250, 0);
+            this.panelDesktop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(1022, 115);
+            this.panelDesktop.TabIndex = 1;
+            // 
+            // resetHome
+            // 
+            this.resetHome.BackColor = System.Drawing.Color.MidnightBlue;
+            this.resetHome.ForeColor = System.Drawing.Color.MediumOrchid;
+            this.resetHome.IconChar = FontAwesome.Sharp.IconChar.House;
+            this.resetHome.IconColor = System.Drawing.Color.MediumOrchid;
+            this.resetHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.resetHome.IconSize = 83;
+            this.resetHome.Location = new System.Drawing.Point(27, 18);
+            this.resetHome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resetHome.Name = "resetHome";
+            this.resetHome.Size = new System.Drawing.Size(86, 83);
+            this.resetHome.TabIndex = 1;
+            this.resetHome.TabStop = false;
+            this.resetHome.Click += new System.EventHandler(this.resetHome_Click);
+            // 
+            // panelChildForm_AD
+            // 
+            this.panelChildForm_AD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panelChildForm_AD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChildForm_AD.Location = new System.Drawing.Point(250, 115);
+            this.panelChildForm_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelChildForm_AD.Name = "panelChildForm_AD";
+            this.panelChildForm_AD.Size = new System.Drawing.Size(1022, 935);
+            this.panelChildForm_AD.TabIndex = 2;
+            this.panelChildForm_AD.Paint += new System.Windows.Forms.PaintEventHandler(this.panelChildForm_AD_Paint);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Indigo;
+            this.panelMenu.BackgroundImage = global::QLCHXM.Properties.Resources.Screenshot_2024_01_17_233258;
+            this.panelMenu.Controls.Add(this.btn_khachhang_AD);
+            this.panelMenu.Controls.Add(this.btn_nhacungcap_AD);
+            this.panelMenu.Controls.Add(this.btn_donhang_AD);
+            this.panelMenu.Controls.Add(this.btn_thongke_AD);
+            this.panelMenu.Controls.Add(this.btn_nhanvien_AD);
+            this.panelMenu.Controls.Add(this.btn_hanghoa_AD);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Controls.Add(this.iconButtonThoat);
+            this.panelMenu.Controls.Add(this.btnDangXuat);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(250, 1050);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btn_khachhang_AD
+            // 
+            this.btn_khachhang_AD.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_khachhang_AD.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_khachhang_AD.ForeColor = System.Drawing.Color.White;
+            this.btn_khachhang_AD.Location = new System.Drawing.Point(0, 434);
+            this.btn_khachhang_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_khachhang_AD.Name = "btn_khachhang_AD";
+            this.btn_khachhang_AD.Size = new System.Drawing.Size(246, 86);
+            this.btn_khachhang_AD.TabIndex = 10;
+            this.btn_khachhang_AD.Text = "Khách hàng";
+            this.btn_khachhang_AD.UseVisualStyleBackColor = false;
+            this.btn_khachhang_AD.Click += new System.EventHandler(this.btn_khachhang_AD_Click);
+            // 
+            // btn_nhacungcap_AD
+            // 
+            this.btn_nhacungcap_AD.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_nhacungcap_AD.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nhacungcap_AD.ForeColor = System.Drawing.Color.White;
+            this.btn_nhacungcap_AD.Location = new System.Drawing.Point(2, 628);
+            this.btn_nhacungcap_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_nhacungcap_AD.Name = "btn_nhacungcap_AD";
+            this.btn_nhacungcap_AD.Size = new System.Drawing.Size(246, 86);
+            this.btn_nhacungcap_AD.TabIndex = 13;
+            this.btn_nhacungcap_AD.Text = "Nhà cung cấp";
+            this.btn_nhacungcap_AD.UseVisualStyleBackColor = false;
+            this.btn_nhacungcap_AD.Click += new System.EventHandler(this.btn_nhacungcap_AD_Click);
+            // 
+            // btn_donhang_AD
+            // 
+            this.btn_donhang_AD.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_donhang_AD.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_donhang_AD.ForeColor = System.Drawing.Color.White;
+            this.btn_donhang_AD.Location = new System.Drawing.Point(2, 338);
+            this.btn_donhang_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_donhang_AD.Name = "btn_donhang_AD";
+            this.btn_donhang_AD.Size = new System.Drawing.Size(246, 86);
+            this.btn_donhang_AD.TabIndex = 9;
+            this.btn_donhang_AD.Text = "Đơn hàng";
+            this.btn_donhang_AD.UseVisualStyleBackColor = false;
+            this.btn_donhang_AD.Click += new System.EventHandler(this.btn_donhang_AD_Click);
+            // 
+            // btn_thongke_AD
+            // 
+            this.btn_thongke_AD.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_thongke_AD.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thongke_AD.ForeColor = System.Drawing.Color.White;
+            this.btn_thongke_AD.Location = new System.Drawing.Point(-2, 723);
+            this.btn_thongke_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_thongke_AD.Name = "btn_thongke_AD";
+            this.btn_thongke_AD.Size = new System.Drawing.Size(246, 86);
+            this.btn_thongke_AD.TabIndex = 12;
+            this.btn_thongke_AD.Text = "Thống kê";
+            this.btn_thongke_AD.UseVisualStyleBackColor = false;
+            this.btn_thongke_AD.Click += new System.EventHandler(this.btn_thongke_AD_Click);
+            // 
+            // btn_nhanvien_AD
+            // 
+            this.btn_nhanvien_AD.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_nhanvien_AD.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nhanvien_AD.ForeColor = System.Drawing.Color.White;
+            this.btn_nhanvien_AD.Location = new System.Drawing.Point(0, 529);
+            this.btn_nhanvien_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_nhanvien_AD.Name = "btn_nhanvien_AD";
+            this.btn_nhanvien_AD.Size = new System.Drawing.Size(246, 86);
+            this.btn_nhanvien_AD.TabIndex = 11;
+            this.btn_nhanvien_AD.Text = "Nhân viên";
+            this.btn_nhanvien_AD.UseVisualStyleBackColor = false;
+            this.btn_nhanvien_AD.Click += new System.EventHandler(this.btn_nhanvien_AD_Click);
+            // 
+            // btn_hanghoa_AD
+            // 
+            this.btn_hanghoa_AD.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_hanghoa_AD.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_hanghoa_AD.ForeColor = System.Drawing.Color.White;
+            this.btn_hanghoa_AD.Location = new System.Drawing.Point(3, 243);
+            this.btn_hanghoa_AD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_hanghoa_AD.Name = "btn_hanghoa_AD";
+            this.btn_hanghoa_AD.Size = new System.Drawing.Size(246, 86);
+            this.btn_hanghoa_AD.TabIndex = 8;
+            this.btn_hanghoa_AD.Text = "Điện thoại";
+            this.btn_hanghoa_AD.UseVisualStyleBackColor = false;
+            this.btn_hanghoa_AD.Click += new System.EventHandler(this.btn_hanghoa_AD_Click_1);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackgroundImage = global::QLCHXM.Properties.Resources.Screenshot_2024_01_17_2332581;
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(248, 234);
+            this.panelLogo.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(65, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Admin";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QLCHXM.Properties.Resources.icon_user_slide__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(72, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 111);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // iconButtonThoat
+            // 
+            this.iconButtonThoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButtonThoat.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButtonThoat.ForeColor = System.Drawing.Color.White;
+            this.iconButtonThoat.IconChar = FontAwesome.Sharp.IconChar.Xmark;
+            this.iconButtonThoat.IconColor = System.Drawing.Color.White;
+            this.iconButtonThoat.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButtonThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButtonThoat.Location = new System.Drawing.Point(0, 978);
+            this.iconButtonThoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.iconButtonThoat.Name = "iconButtonThoat";
+            this.iconButtonThoat.Size = new System.Drawing.Size(244, 72);
+            this.iconButtonThoat.TabIndex = 6;
+            this.iconButtonThoat.Text = "Thoát";
+            this.iconButtonThoat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButtonThoat.UseVisualStyleBackColor = true;
+            this.iconButtonThoat.Click += new System.EventHandler(this.iconButtonThoat_Click);
+            // 
+            // btnDangXuat
+            // 
+            this.btnDangXuat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangXuat.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangXuat.ForeColor = System.Drawing.Color.White;
+            this.btnDangXuat.IconChar = FontAwesome.Sharp.IconChar.ArrowAltCircleRight;
+            this.btnDangXuat.IconColor = System.Drawing.Color.White;
+            this.btnDangXuat.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnDangXuat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangXuat.Location = new System.Drawing.Point(4, 844);
+            this.btnDangXuat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDangXuat.Name = "btnDangXuat";
+            this.btnDangXuat.Size = new System.Drawing.Size(242, 124);
+            this.btnDangXuat.TabIndex = 6;
+            this.btnDangXuat.Text = "Đăng xuất";
+            this.btnDangXuat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDangXuat.UseVisualStyleBackColor = true;
+            this.btnDangXuat.Click += new System.EventHandler(this.btnDangXuat_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(1272, 1050);
+            this.Controls.Add(this.panelChildForm_AD);
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panelMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.panelDesktop.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resetHome)).EndInit();
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private FontAwesome.Sharp.IconButton iconButtonThoat;
+        private FontAwesome.Sharp.IconButton btnDangXuat;
+        private System.Windows.Forms.Panel panelDesktop;
+        private System.Windows.Forms.Panel panelChildForm_AD;
+        private FontAwesome.Sharp.IconPictureBox resetHome;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button btn_hanghoa_AD;
+        private System.Windows.Forms.Button btn_thongke_AD;
+        private System.Windows.Forms.Button btn_nhanvien_AD;
+        private System.Windows.Forms.Button btn_khachhang_AD;
+        private System.Windows.Forms.Button btn_donhang_AD;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_nhacungcap_AD;
+    }
+}
+
---
+++ Form Trang Chủ/Home.cs
@@ -0,0 +1,139 @@
+﻿using QLCHXM.DangNhap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+using System.Security.Cryptography;
+
+namespace QLCHXM
+{
+    public partial class Home : Form
+    {
+
+        
+        public Home()
+        {
+            InitializeComponent();
+ 
+           
+        }
+        private Form activeform = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeform != null)
+                activeform.Close();
+            activeform = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm_AD.Controls.Add(childForm);
+            panelChildForm_AD.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+        private Button currentButton;
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = ColorTranslator.FromHtml("#4169E1");
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(39, 39, 58);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                }
+            }
+        }
+        private void Home_Load(object sender, EventArgs e)
+        {
+            Class.Functions.Connect();
+
+            btn_hanghoa_AD.PerformClick();
+
+            
+        }
+
+        private void iconButtonThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDangXuat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn đăng xuất!", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                this.Hide();
+                Form_Dang_Nhap form_Dang_Nhap = new Form_Dang_Nhap();
+                form_Dang_Nhap.ShowDialog();
+            }
+        }
+        
+        
+        private void resetHome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Home home = new Home();
+            home.ShowDialog();
+        }
+
+        private void btn_hanghoa_AD_Click_1(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            openChildForm(new HangHoa());
+        }
+
+        private void btn_donhang_AD_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            openChildForm(new DonHang());
+        }
+
+        private void btn_khachhang_AD_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            openChildForm(new fKhachHang());
+        }
+
+        private void btn_nhanvien_AD_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            openChildForm(new NhanVien());
+        }
+
+        private void btn_thongke_AD_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            openChildForm(new fThongKe());
+        }
+
+        private void btn_nhacungcap_AD_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            openChildForm(new NhaCungCap());
+        }
+
+        private void panelChildForm_AD_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
